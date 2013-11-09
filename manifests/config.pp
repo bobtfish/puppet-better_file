9a02b9430605f0acaf62c852c89e5acad7096737
Tomas Doran <tdoran@yelp.com>
2013-11-09 15:21:25 +0000

    Remove unused manifests

--- manifests/config.pp
+++
@@ -1,6 +0,0 @@
-# == Class better_file::config
-#
-# This class is called from better_file
-#
-class better_file::config {
-}
--- manifests/install.pp
+++
@@ -1,9 +0,0 @@
-# == Class better_file::intall
-#
-class better_file::install {
-  include better_file::params
-
-  package { $better_file::params::package_name:
-    ensure => present,
-  }
-}
